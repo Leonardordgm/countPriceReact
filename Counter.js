23bdc215be91069a5131e514688df3076cb3741d
Leonardo Rodrigues Martins <117056433+Leonardordgm@users.noreply.github.com>
2023-08-26 16:07:07 -0300

    Update Counter.js

--- Counter.js
+++ Counter.js
@@ -1,1 +1,19 @@
+function MyButton({ count, onClick }) {
+    
 
+    return (
+      <>
+      { count < 10 ? (<button onClick={onClick}>
+         {count} 
+      </button>)
+    : (
+    <button>
+      10. 
+   </button>)
+    }
+    </>
+    )
+
+  }
+
+export default MyButton
